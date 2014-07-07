b16a02f9e2206dc63a53d2255c7c26128326bded
Alexwiththescar <thesalesstar@gmail.com>
2014-07-07 22:01:53 +0200

    nes

--- app/models/leads_a.rb
+++ app/models/leads_a.rb
@@ -2,7 +2,7 @@ class LeadsA < ActiveRecord::Base
   attr_accessible :Distance, :Price, :latitude, :longitude, :Address1, :Address2, :Address3, :Address4, :Address5, :Address6, :FirstName, :HomePhone, :LastName, :Postcode, :Price, :Title
   geocoded_by :Postcode   
   after_validation :geocode
-  #acts_as_gmappable 
+  acts_as_gmappable 
 
 def self.import(file)
   CSV.foreach(file.path, headers: true) do |row|
