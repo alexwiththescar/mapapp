5ff3f73121df64ed5db24954ebdaf8a6bcdd0841
Alexwiththescar <thesalesstar@gmail.com>
2014-07-09 16:27:08 +0200

    leadsB

--- app/models/leads_b.rb
+++ app/models/leads_b.rb
@@ -2,11 +2,9 @@ class LeadsB < ActiveRecord::Base
   attr_accessible :Distance, :Price, :latitude, :longitude, :Address1, :Address2, :Address3, :Address4, :Address5, :Address6, :FirstName, :HomePhone, :LastName, :Postcode, :Price, :Title
   geocoded_by :Postcode   
   after_validation :geocode
-  acts_as_gmappable 
+  acts_as_gmappable :process_geocoding => false 
 
-  def gmaps4rails_address 
-      	postcode
-     	end
+ 
 
 def self.import(file)
   CSV.foreach(file.path, headers: true) do |row|
