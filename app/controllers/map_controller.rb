681de7a0d7e938b2584ac40a721cf4386810dc37
Alexwiththescar <thesalesstar@gmail.com>
2014-07-10 14:26:33 +0200

    removed listings on map dshow

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -16,30 +16,30 @@ end
 @hashB = LeadsA.all.to_gmaps4rails do |lead, marker|
    lead.latitude
    lead.longitude
-   marker.infowindow lead.LastName
-   marker.picture({
-   "picture" => "../assets/map_green.png",
-   "width" =>  '10',        
-   "height" => '10'
-  })
+   marker.infowindow lead.Postcode
+    marker.picture({
+    "picture" => "../assets/map_green.png",
+    "width" =>  '10',        
+    "height" => '10'
+   })
 end
 
 @hashC = LeadsB.all.to_gmaps4rails do |lead, marker|
    lead.latitude
    lead.longitude
-   marker.infowindow lead.LastName
-   marker.picture({
-   "picture" => "../assets/map_blur.png",
-   "width" =>  '10',        
-   "height" => '10'
-  })
+   marker.infowindow lead.Postcode
+    marker.picture({
+    "picture" => "../assets/map_blur.png",
+    "width" =>  '10',        
+    "height" => '10'
+   })
 end
 
 @map = @hashA + @hashB + @hashC
 ### for some reason, when adding the two arrays, to_gmaps4rails seems to make a string, so we 
 ### need to remove the array brackets after we concat the string. Hacky way, but it works!
 @map.gsub! "][", ","
-## We now have an array of hashes @hashC! Yay!
+## We now have an array of hashes @map! Yay!
 
 
   end 
