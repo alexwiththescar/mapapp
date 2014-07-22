4f8ffd16e38c0f7076c9a580ef7a7f009d230bf2
Alexwiththescar <thesalesstar@gmail.com>
2014-07-22 14:00:30 +0200

    mapapp on EC2

--- app/models/leads_a.rb
+++ app/models/leads_a.rb
@@ -24,7 +24,7 @@
 #
 
 class LeadsA < ActiveRecord::Base
-  attr_accessible :Distance, :Price, :latitude, :longitude, :Address1, :Address2, :Address3, :Address4, :Address5, :Address6, :FirstName, :HomePhone, :LastName, :Postcode, :Price, :Title
+  attr_accessible :venue, :Distance, :Price, :latitude, :longitude, :Address1, :Address2, :Address3, :Address4, :Address5, :Address6, :FirstName, :HomePhone, :LastName, :Postcode, :Price, :Title
   geocoded_by :Postcode   
   after_validation :geocode
   acts_as_gmappable :process_geocoding => false
