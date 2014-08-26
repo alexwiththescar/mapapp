e659d297874dcc18b88faf1405502ccf8b86a55a
Alexwiththescar <thesalesstar@gmail.com>
2014-08-26 14:04:02 +0200

    removed threading

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,10 +2,10 @@ class Person < ActiveRecord::Base
   attr_accessible :count, :postcode
 
 def self.import(file)
-	Thread.new do
+	
   CSV.foreach(file.path, headers: true) do |row|
     Person.create! row.to_hash
-  end
+
 end
 end
 
